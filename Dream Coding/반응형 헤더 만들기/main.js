193dd2c93ed62d77513b1fb2c10bc5330b618beb
sujeong054 <111235507+sujeong054@users.noreply.github.com>
2022-10-13 23:25:51 +0900

    Finish
    js 부분 빼고 혼자서 완성!
    html 파일 / 노션에도 트러블 슈팅 기재했다... 이제 티스토리 말고 노션에 잘 기록해봐야지
    js toggle 부분 잘 모르겠다 다음에 자세히 공부해야지

---
+++ Dream Coding/반응형 헤더 만들기/main.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector('.btn');
+const menu = document.querySelector('.menu');
+const icons = document.querySelector('.icons');
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
+} );
